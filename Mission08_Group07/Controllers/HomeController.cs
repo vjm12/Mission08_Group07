62275fdd96e52f777a20278a1873abbbca31d9e3
vjm12 <vjm12@byu.edu>
2023-02-23 14:05:48 -0700

    Added Add/EditTask view

--- Mission08_Group07/Controllers/HomeController.cs
+++ Mission08_Group07/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace Mission08_Group07.Controllers
         {
             return View();
         }
+        public IActionResult AddEditTask()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
