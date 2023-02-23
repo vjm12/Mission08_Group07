335dafdc569c5fcd71a01e1c37d0ca083af66b02
vjm12 <vjm12@byu.edu>
2023-02-23 12:59:44 -0700

    Add project files.

---
+++ Mission08_Group07/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mission08_Group07.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
